f8c59a1a2a05a44cb18b5acf4c7e6f8b1bc1bbd1
banzai.ko <boxko398@gmail.com>
2024-11-18 23:51:58 +0000

    Punto c

--- 2doParcial/1-runVms.sh
+++ 2doParcial/1-runVms.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
+# Key Generation
+cd ../VMs/keys
+ssh-keygen -t ed25519 -C "boxko398@gmail.com" -f ~/.ssh/key
+
 # Controller Node
-cd ../VMs/vagrant_web
+cd ../vagrant_web
 vagrant plugin install vagrant-disksize
 export VAGRANT_EXPERIMENTAL="1"
 vagrant up
